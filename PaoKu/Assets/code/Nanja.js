a41f1419d34d241521387c351022da4701787b11
yxh <2833144567@qq.com>
2017-02-15 00:01:44 +0800

    add game paoku

---
+++ PaoKu/Assets/code/Nanja.js
@@ -0,0 +1,26 @@
+﻿#pragma strict
+
+var animator:Animator;
+
+function Start () {
+	animator = GetComponent("Animator");
+}
+
+function FixedUpdate(){
+	MoveCharacter();
+}
+function MoveCharacter(){
+	animator.SetBool("Run",true);
+	if(Input.GetKey(KeyCode.W)){
+	animator.SetBool("Jump",true);
+	animator.SetBool("Run",false);
+	}else{
+	animator.SetBool("Jump",false);
+	}
+	if(Input.GetKey(KeyCode.S)){
+	animator.SetBool("Slide",true);
+	animator.SetBool("Run",false);
+	}else{
+	animator.SetBool("Slide",false);
+	}
+}
